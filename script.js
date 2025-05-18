9270880a6549ff8794b41a9d756bf1004ec8607d
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-05-18 20:56:11 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var num = document.querySelector(".inumber")
 var adc = document.querySelector(".adc")
 var sele = document.querySelector(".ilist")
+var fim = document.querySelector(".fim")
+
+function enviar(){
+    if (num.lenght)
+}
