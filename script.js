e53e825355532ae5c9bfe21025ac4a0a29fecc1f
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-05-18 21:23:30 -0300

    up

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var fim = document.querySelector(".fim")
 function enviar(){
     if (num.value.length == 0 || num.value < 1 || num.value > 100){
         alert("Valor invalido")
+    }else{
+        
     }
 }
 
