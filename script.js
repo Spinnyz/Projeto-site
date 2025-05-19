4b1966e564d089a428a7d4e1c50a890d2e1c0bc8
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-05-19 20:11:49 -0300

    up

--- script.js
+++ script.js
@@ -23,7 +23,15 @@ function finalizar(){
     var quantidade = sele.length 
     var maior = MathMLElement.max(valores)
     var menor = MathMLElement.min(valores)
-    var 
+    let soma = valores.reduce((a, b) => a + b, 0);
+     let media = soma / total;
+
+    resultado.innerHTML = "";
+    resultado.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`;
+    resultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+    resultado.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
+    resultado.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`;
+    resultado.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`;
 }
     
     
