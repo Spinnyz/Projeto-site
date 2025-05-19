65a00a864adec49b7449062c9fa414c93a177e9e
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-05-19 20:13:13 -0300

    up

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-var num = document.querySelector("#inumber")
-var adc = document.querySelector(".adc")
-var sele = document.querySelector("#ilist")
-var fim = document.querySelector(".fim")
-var res = document.querySelector(".resul")
-var valores = []
+var num = document.querySelector("#inumber");
+var adc = document.querySelector(".adc");
+var sele = document.querySelector("#ilist");
+var fim = document.querySelector(".fim");
+var res = document.querySelector(".resul");
+var valores = [];
 
-function adicionar(){
+function adicionar() {
+    if (num.value.length == 0 || num.value < 1 || num.value > 100) {
+        alert("Valor inválido");
+    } else {
+        var valor = Number(num.value);
 
-if (num.value.length == 0 || num.value < 1 || num.value > 100){
-    alert("Valor invalido")
-}else{
-    var valor = Number(num.value)
-   var numa = document.createElement("option")
-    numa.innerHTML = `Valor ${valor} adicionado`  
-    sele.appendChild(numa)
+        if (valores.includes(valor)) {
+            alert("Valor já adicionado");
+        } else {
+            valores.push(valor);
+            var numa = document.createElement("option");
+            numa.innerHTML = `Valor ${valor} adicionado`;
+            sele.appendChild(numa);
+            res.innerHTML = ""; // Limpa os resultados ao adicionar novo
+        }
+    }
+
+    num.value = "";
+    num.focus();
 }
-}
 
-adc.addEventListener("click",adicionar)
+adc.addEventListener("click", adicionar);
 
-function finalizar(){
-    var quantidade = sele.length 
-    var maior = MathMLElement.max(valores)
-    var menor = MathMLElement.min(valores)
+function finalizar() {
+    if (valores.length === 0) {
+        alert("Adicione valores antes de finalizar");
+        return;
+    }
+
+    let total = valores.length;
+    let maior = Math.max(...valores);
+    let menor = Math.min(...valores);
     let soma = valores.reduce((a, b) => a + b, 0);
-     let media = soma / total;
+    let media = soma / total;
 
-    resultado.innerHTML = "";
-    resultado.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`;
-    resultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
-    resultado.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
-    resultado.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`;
-    resultado.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`;
+    res.innerHTML = "";
+    res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`;
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`;
+    res.innerHTML += `<p>A média dos valores digitados é ${media.toFixed(2)}.</p>`;
 }
-    
-    
-fim.addEventListener("click",finalizar)
+
+fim.addEventListener("click", finalizar);
